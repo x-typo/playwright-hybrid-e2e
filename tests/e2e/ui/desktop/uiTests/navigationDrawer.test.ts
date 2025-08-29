9ca93a0d9bec25628e23832843b698b6ce4f705b
x-typo <87083014+x-typo@users.noreply.github.com>
2025-08-29 12:14:11 -0600

    Remove .only from Drawer Validation test to run all tests

--- tests/e2e/ui/desktop/uiTests/navigationDrawer.test.ts
+++ tests/e2e/ui/desktop/uiTests/navigationDrawer.test.ts
@@ -19,7 +19,7 @@ test.beforeEach("Setup", async ({ page, navigationDrawer }) => {
   }
 });
 
-test.only("Drawer Validation", async ({ navigationDrawer }) => {
+test("Drawer Validation", async ({ navigationDrawer }) => {
   await test.step("Verify", async () => {
     await navigationDrawer.verifyDrawer();
   });
