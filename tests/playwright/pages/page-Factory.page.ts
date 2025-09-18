fb6e4a01120a02c335d4e0ea2ad535f369807065
x-typo <87083014+x-typo@users.noreply.github.com>
2025-09-18 11:55:07 -0600

    refactor: simplify PageFactory by removing unused methods and optimizing constructor

--- tests/playwright/pages/page-Factory.page.ts
+++ tests/playwright/pages/page-Factory.page.ts
@@ -1,52 +1,18 @@
 import { Page } from "@playwright/test";
-import { LoginPage } from "../tests/playwright/pages/login.page";
-import { NavigationDrawer } from "../tests/playwright/pages/navigation-drawer.page";
-import { CustomersListPage } from "../tests/playwright/pages/customers-list.page";
-import { NotesDashboardPage } from "../tests/playwright/pages/notes-dashboard.page";
-import { ModalsPage } from "../tests/playwright/pages/modals.page";
-import { BasePage } from "../tests/playwright/pages/base.page";
+import { LoginPage } from "./login.page";
+import { NotesDashboardPage } from "./notes-dashboard.page";
 
 export class PageFactory {
-  private page: Page;
-  private isMobile: boolean;
-  private instances = new Map<string, any>();
+  constructor(
+    private page: Page,
+    private isMobile: boolean | undefined
+  ) {}
 
-  constructor(page: Page, isMobile: boolean) {
-    this.page = page;
-    this.isMobile = isMobile;
+  getLoginPage(): LoginPage {
+    return new LoginPage(this.page, this.isMobile);
   }
 
-  getLoginPage() {
-    return this.get("loginPage", LoginPage);
-  }
-
-  getNavigationDrawer() {
-    return this.get("navigationDrawer", NavigationDrawer);
-  }
-
-  getCustomersListPage() {
-    return this.get("customersListPage", CustomersListPage);
-  }
-
-  getNotesDashboardPage() {
-    return this.get("notesDashboardPage", NotesDashboardPage);
-  }
-
-  getModalsPage() {
-    return this.get("modalsPage", ModalsPage);
-  }
-
-  getBasePage() {
-    return this.get("basePage", BasePage);
-  }
-
-  private get<T>(
-    key: string,
-    Type: new (page: Page, isMobile: boolean) => T
-  ): T {
-    if (!this.instances.has(key)) {
-      this.instances.set(key, new Type(this.page, this.isMobile));
-    }
-    return this.instances.get(key);
+  getNotesDashboardPage(): NotesDashboardPage {
+    return new NotesDashboardPage(this.page, this.isMobile);
   }
 }
