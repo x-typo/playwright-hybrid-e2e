aa8916f9f8ba5dbe37aa81383f485a91fc49a4a0
x-typo <87083014+x-typo@users.noreply.github.com>
2025-09-18 11:42:29 -0600

    refactor: convert locator properties to getter methods in LoginPage

--- tests/playwright/pages/login.page.ts
+++ tests/playwright/pages/login.page.ts
@@ -2,29 +2,34 @@ import { type Locator, type Page } from "@playwright/test";
 import { BasePage } from "./base.page";
 
 export class LoginPage extends BasePage {
-  readonly loginPageHeading: Locator;
-  readonly emailInputBox: Locator;
-  readonly passwordInputBox: Locator;
-  readonly loginButton: Locator;
-  readonly googleLoginButton: Locator;
-
   constructor(page: Page, isMobile: boolean | undefined) {
     super(page, isMobile);
+  }
 
-    this.loginPageHeading = this.heading("Login");
-    this.emailInputBox = this.inputBox("Email");
-    this.passwordInputBox = this.inputBox("Password");
-    this.loginButton = this.testIdSelector("login-submit");
-    this.googleLoginButton = this.testIdSelector("login-with-google");
+  // ===== LOCATOR GETTERS =====
+  get loginPageHeading(): Locator {
+    return this.heading("Login");
+  }
+  get emailInputBox(): Locator {
+    return this.inputBox("Email");
+  }
+  get passwordInputBox(): Locator {
+    return this.inputBox("Password");
+  }
+  get loginButton(): Locator {
+    return this.testIdSelector("login-submit");
+  }
+  get googleLoginButton(): Locator {
+    return this.testIdSelector("login-with-google");
   }
 
   // ===== NAVIGATION =====
-  async navigateLoginPage() {
+  async navigateLoginPage(): Promise<void> {
     await this.navigatePage("/notes/app/login");
   }
 
   // ===== INTERACTIONS =====
-  async selectLoginButton() {
+  async selectLoginButton(): Promise<void> {
     await this.loginButton.click();
   }
 
@@ -34,7 +39,7 @@ export class LoginPage extends BasePage {
   }: {
     emailAddress: string;
     password: string;
-  }) {
+  }): Promise<void> {
     await this.emailInputBox.fill(emailAddress);
     await this.passwordInputBox.fill(password);
     await this.loginButton.click();
