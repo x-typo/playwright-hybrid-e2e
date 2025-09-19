dd72a3a9e9f09e4c0b6451f96b9e1efbe250c902
x-typo <87083014+x-typo@users.noreply.github.com>
2025-09-19 10:06:55 -0600

    refactor: comment out deleteNoteById method and update import path for API endpoints

--- api/clients/notes.client.ts
+++ api/clients/notes.client.ts
@@ -1,12 +1,12 @@
 import { APIRequestContext } from "@playwright/test";
-import { API_ENDPOINTS } from "../routes/endpoints";
+import { NOTES_ENDPOINTS as API_ENDPOINTS } from "../endpoints/notes-endpoints";
 
 export class NotesClient {
   constructor(private request: APIRequestContext) {}
 
-  async deleteNoteById(noteId: string) {
-    const endpointPath = API_ENDPOINTS.notes.deleteById(noteId);
-    const response = await this.request.delete(endpointPath);
-    return response;
-  }
+  // async deleteNoteById(noteId: string) {
+  //   const endpointPath = API_ENDPOINTS.notes.deleteById(noteId);
+  //   const response = await this.request.delete(endpointPath);
+  //   return response;
+  // }
 }
