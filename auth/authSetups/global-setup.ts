797df0d96d9d512ac6c1170c1699741de3590b0e
x-typo <87083014+x-typo@users.noreply.github.com>
2025-09-20 09:07:10 -0600

    feat: add health, notes, and users endpoints with respective API paths

--- auth/authSetups/global-setup.ts
+++ auth/authSetups/global-setup.ts
@@ -3,7 +3,7 @@ import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
-import { USERS_ENDPOINTS } from "../../api/endpoints/users-xndpoints";
+import { USERS_ENDPOINTS } from "../../api/endpoints/users-endpoints";
 
 dotenv.config();
 
