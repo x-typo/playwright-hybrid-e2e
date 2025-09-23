fa86d51bf44df26042cb110e7799aac3a5b69559
x-typo <87083014+x-typo@users.noreply.github.com>
2025-09-23 11:16:27 -0600

    Refactor addNewNote method to simplify note creation process by removing unnecessary API response handling

--- tests/playwright/pages/notes-dashboard.page.ts
+++ tests/playwright/pages/notes-dashboard.page.ts
@@ -77,32 +77,10 @@ export class NotesDashboardPage extends BasePage {
     await this.tabButton(name).click();
   }
 
-  // async addNewNote(title: string, description: string): Promise<string> {
-  //   const responsePromise = this.page.waitForResponse(
-  //     (response) =>
-  //       response.url().endsWith(API_ENDPOINTS.notes.create) &&
-  //       response.request().method() === "POST"
-  //   );
-
-  //   await this.addNoteButton.click();
-  //   await this.addNoteTitleInputBox.fill(title);
-  //   await this.addNoteDescriptionInputBox.fill(description);
-  //   await this.submitButton.click();
-
-  //   const response = await responsePromise;
-  //   const responseBody = await response.json();
-
-  //   expect(response.ok(), "The 'create note' API call failed.").toBe(true);
-
-  //   const {
-  //     data: { id: noteId },
-  //   } = responseBody;
-
-  //   expect(
-  //     noteId,
-  //     "Could not find 'id' in the API response body."
-  //   ).toBeDefined();
-
-  //   return noteId;
-  // }
+  async addNewNote(title: string, description: string) {
+    await this.addNoteButton.click();
+    await this.addNoteTitleInputBox.fill(title);
+    await this.addNoteDescriptionInputBox.fill(description);
+    await this.submitButton.click();
+  }
 }
