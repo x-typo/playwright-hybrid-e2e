3f034e4f1cef554859a6a3e8351ab16825d8b08a
x-typo <87083014+x-typo@users.noreply.github.com>
2025-09-23 14:27:07 -0600

    Refactor getNoteIdByTitle function to accept a flat notes array instead of a nested array

--- api/utils/notes-helpers.ts
+++ api/utils/notes-helpers.ts
@@ -1,10 +1,6 @@
 import { Note } from "../models/notes.models";
 
-export function getNoteIdByTitle(
-  notes: Note[][],
-  title: string
-): string | null {
-  const allNotes = notes.flat();
-  const foundNote = allNotes.find((n) => n.title === title);
+export function getNoteIdByTitle(notes: Note[], title: string): string | null {
+  const foundNote = notes.find((n) => n.title === title);
   return foundNote ? foundNote.id : null;
 }
